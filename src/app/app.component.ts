9b61535d2f48acf90d2e63bb451355327436a1bb
Paweł Janowicz <janowiczpawel4@gmail.com>
2024-11-08 11:11:46 +0100

    adding form module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SendMessageFormComponent} from './send-message-form/send-message-form.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SendMessageFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
