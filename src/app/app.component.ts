18840324f2c999242c37b2e8c877d02f5ce96ebe
Paweł Janowicz <janowiczpawel4@gmail.com>
2024-11-08 15:36:51 +0100

    adding new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {SendMessageFormComponent} from './send-message-form/send-message-form.component';
+import {SendMessageFormComponent} from './contact/send-message-form.component';
+import {PricingComponent} from './pricing/pricing.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SendMessageFormComponent],
+  imports: [RouterOutlet, SendMessageFormComponent, PricingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/contact/send-message-form.component.ts
+++ src/app/contact/send-message-form.component.ts
@@ -1,22 +1,33 @@
 import { Component } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-send-message-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './send-message-form.component.html',
 })
 export class SendMessageFormComponent {
   messageForm: FormGroup;
+  title: string = 'Send Message';
 
   constructor(private http: HttpClient) {
     this.messageForm = new FormGroup({
       name: new FormControl('', Validators.required),
       phoneNumber: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
-      message: new FormControl('', Validators.required)
+      message: new FormControl('', Validators.required),
     });
   }
 
