87600336149ee532e7c3ac882990ce2c301c1d9c
Paweł Janowicz <janowiczpawel4@gmail.com>
2024-11-08 19:40:06 +0100

    adding new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import {SendMessageFormComponent} from './contact/send-message-form.component';
 import {PricingComponent} from './pricing/pricing.component';
 import {NavbarComponent} from './navbar/navbar.component';
+import {MatIcon} from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SendMessageFormComponent, PricingComponent, NavbarComponent],
+  imports: [RouterOutlet, SendMessageFormComponent, PricingComponent, NavbarComponent, MatIcon],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
