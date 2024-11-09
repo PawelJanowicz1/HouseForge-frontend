a122cccce219e279f1ad5c731a506e98133cfea3
Paweł Janowicz <janowiczpawel4@gmail.com>
2024-11-09 21:03:10 +0100

    making improvements with landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {RouterModule, RouterOutlet} from '@angular/router';
+import {Router, RouterModule, RouterOutlet} from '@angular/router';
 import {SendMessageFormComponent} from './contact/send-message-form.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {MatIcon} from '@angular/material/icon';
@@ -14,5 +14,11 @@ import {AboutComponent} from './about/about.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor(private router: Router) {}
   title = 'HouseForge-frontend';
+
+  isAboutPage(): boolean {
+    return this.router.url === '/about';
+  }
+
 }
