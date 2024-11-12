b87a3d71afc869a31867f7746c6cc79f4676c659
Paweł Janowicz <janowiczpawel4@gmail.com>
2024-11-12 17:37:26 +0100

    adding new improvements in contact section

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+export const API_CONFIG = {
+  backendUrl: 'http://budowa-domow-trojmiasto.pl:8086',
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    { provide: 'API_CONFIG', useValue: API_CONFIG }
+  ]
 };
